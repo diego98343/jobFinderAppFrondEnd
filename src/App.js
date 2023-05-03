06e094c9b0819b17b3b8aedfc49536af260ef239
diegocolonia <diegocolonia98@yahoo.com>
2023-05-03 13:40:15 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import {Landing,Error,Dashboard,Register} from './pages'
-
+import Logo from './components/Logo'
 
 function App() {
   return (
       <BrowserRouter>
+      <Logo></Logo>
           <Routes>
              <Route path='/' element={<Dashboard/>}/>
              <Route path='/landing' element={<Landing/>}/>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import img from '../assets/images/not-found.svg'
+import Wrapper from '../assets/wrappers/ErrorPage'
 function Error() {
   return (
-    <div>Error</div>
+    <Wrapper className="full-page">
+        <div>
+            <img src={img}  alt='not found'/>
+            <h3>ohh! Page Not Found</h3>
+            <p>We cant seem to find the page you're looking for</p>
+            <Link to='/'>back home</Link>
+        </div>
+    </Wrapper>
   )
 }
 
