2aa5f4d8da12559033fc846ae3b16e9aedb72bf8
diegocolonia <diegocolonia98@yahoo.com>
2023-05-04 21:07:48 -0400

    commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Logo from './Logo';
+import FormRow from './FormRow';
+
+
+
+
+export {Logo,FormRow};
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,72 @@
+
+
+import {useState,useEffect} from 'react'
 import React from 'react'
+import Wrapper from '../assets/wrappers/RegisterPage';
+import Logo from '../components/Logo'
+import { FormRow } from '../components';
+
+const initialState = {
+    name:'',
+    email:'',
+    password:'',
+    inMember:true
+}
+
+
+
+
+
 
 function Register() {
+
+const [values, setValues] = useState(initialState)
+
+const handleChange = (e) =>{
+    console.log(e.target)
+};
+
+const onSubmit = (e) =>{ 
+    e.preventDefault()
+    console.log(e.target)
+};
+
+
   return (
-    <div>Register</div>
+    <Wrapper>
+           <form className="form" onSubmit={onSubmit}>
+              <Logo></Logo>
+               <h3>Login</h3>
+
+               <FormRow
+                type='text'
+                name='name'
+                value={values.name}
+                handleChange={handleChange}
+               />
+
+              <FormRow
+                type='email'
+                name='email'
+                value={values.email}
+                handleChange={handleChange}
+               />
+               
+               <FormRow
+                type='password'
+                name='password'
+                value={values.password}
+                handleChange={handleChange}
+               />
+
+
+         
+            <button type='submit' className='btn btn-block'>
+                 submit
+              </button>
+           </form>
+    </Wrapper>
+    
   )
 }
 
