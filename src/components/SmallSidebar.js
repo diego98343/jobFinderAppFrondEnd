5c2d1c2de9a006202c19d8203555e9202dc9c0a7
diegocolonia <diegocolonia98@yahoo.com>
2023-05-23 11:45:08 -0400

    commit

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import {FaTimes} from 'react-icons/fa';
+import {NavLink} from 'react-router-dom'
+import Logo from './Logo'
+import {useSelector,useDispatch} from 'react-redux'
 
 
 function SmallSidebar() {
   return (
    <Wrapper>
-        <h3>SmallSidebar</h3>
+     <div className='sidebar-container show-sidebar'>
+      <div className='content'>
+          <button className='close-btn' onClick={()=> console.log('toggle')}>
+            <FaTimes/>
+          </button>
+          <header>
+            <Logo/>
+          </header>
+          <div className='nav-links'>nav links</div>
+      </div>
+
+     </div>
    </Wrapper>
       
   )
