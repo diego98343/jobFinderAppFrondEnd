f33a15571dd0604a9223959d27dca8486309fd11
diegocolonia <diegocolonia98@yahoo.com>
2023-05-25 10:30:38 -0400

    commit

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import {FormRow} from '../../components'
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
+import {useDispatch,useSelector} from 'react-redux'
+import {toast} from 'react-toastify'
 
 function Profile() {
+  const {isLoading} = useSelector((store)=> store.user);
+  const dispatch = useDispatch();
   return (
     <div>Profile</div>
   )
